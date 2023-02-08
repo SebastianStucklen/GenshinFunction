f68d40838a529ea7083f664c8dad2f6dedec32f8
SebastianStucklen <124830101+SebastianStucklen@users.noreply.github.com>
2023-02-08 13:33:13 -0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+//function declaration
+// type name parameter 1 parameter 2
+int ReactMult(int m, int p);
+int main() {
+
+}
+// Write a function that outputs the reaction multiplier in Genshin Impact.
+//
+// Call the function "ReactMult".It returns a floatand takes two character parameters.
+// The first parameter will either be 'm' for melt or 'v' for vaporize.
+// The second will be what's triggered: (p)yro, (c)yro, or (h)ydro.
+// 
+// In the definition, first check that the proper parameters have been passed.
+// If you don't get an 'm' or 'v' for the first one, or a p/c/h for the second, print an error message and return -1. 
+//
+// If the parameters check out, return the following :
+//
+// 2.0 for melt if pyro triggers
+// 1.5 for melt if cyro triggers
+// 2.0 for vaporize if hydro triggers
+// 1.5 for vaporize if pyro triggers
+// Create a loop inside main that continuously asks the user for the parameters neededand call the function, passing it the user's input to test. 
+// The loop should quit if either parameter is 'q'.
+// function defenition
+int ReactMult(int m, int p) {
+
+}
